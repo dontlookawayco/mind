8008c8ceeef987cdf30df6cf1befcab50eeaa864
abcdan <daniel@lngzl.nl>
2021-02-18 00:05:45 +0100

    :sparkles: first homepage

--- mind/pages/index.vue
+++ mind/pages/index.vue
@@ -1,78 +1,9 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        mind
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class="flex h-screen bg-indigo-500 text-center">
+    <div class="bg-white px-10 py-12 rounded-lg m-auto w-1/3">
+      <h1 class="text-3xl font-semibold">Mind <span class="font-light">Safely store information</span></h1>
+      <p class="text-xl font-light my-4">Everything you enter within the application will not leave your device unencrypted.</p>
+      <button class="text-2xl bg-indigo-500 p-3 rounded w-full text-white">Access your vault</button>
     </div>
   </div>
 </template>
-
-<script>
-export default {}
-</script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
