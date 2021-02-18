e075df988fbbb27dd9d70c4e83c40568a513e7c4
abcdan <daniel@lngzl.nl>
2021-02-18 21:03:47 +0100

    :pencil: start with login

---
+++ mind/pages/person/register.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex h-screen bg-indigo-500 text-center">
+    <div class="bg-white px-10 py-12 rounded-lg m-auto w-1/3">
+      <h1 class="text-3xl font-semibold">Mind <span class="font-light">Signup & Start Storing</span></h1>
+      <p class="text-xl font-light my-4">Alright, we're about to start the signup process. It'll take a few minutes.</p>
+      <button class="text-2xl bg-indigo-500 p-3 rounded w-full text-white">Okay, let's start.</button>
+    </div>
+  </div>
+</template>
